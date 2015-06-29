cd6dd8236f373efac20fc828de36177e87f21834
homer <alteslager@gmail.com>
2015-06-29 14:19:27 +0300

    extended search in Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < CrudController
   self.permitted_attrs = [:name, :phone, :about, :email, :latitude, :longitude, :country, :city, :street_address]
-  self.search_columns = [:name]
+  self.search_columns = [:name, :country, :city, :street_address]
 
   def index
     super
