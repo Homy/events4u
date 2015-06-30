d323d6bc9e3d668571055c1beb196dff05caad27
homer <alteslager@gmail.com>
2015-06-30 22:50:08 +0300

    authentication(Devise) + app/assets/javascripts/bootstrap.js(bumped into issue with non-working dropdown menu), also formatted a bit navbar

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'bootswatch-rails'
 gem 'geocoder'
 gem 'gmaps4rails'
 
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629193050) do
+ActiveRecord::Schema.define(version: 20150630172702) do
 
   create_table "event_types", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -74,6 +74,24 @@ ActiveRecord::Schema.define(version: 20150629193050) do
   end
 
   add_index "reports", ["event_id"], name: "index_reports_on_event_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", limit: 255, default: "", null: false
+    t.string "encrypted_password", limit: 255, default: "", null: false
+    t.string "reset_password_token", limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", limit: 4, default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip", limit: 255
+    t.string "last_sign_in_ip", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "events", "event_types"
   add_foreign_key "events", "hosts"
