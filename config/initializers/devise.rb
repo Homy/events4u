132918545369f3d5ea01fb8355dee643b23c9848
homer <alteslager@gmail.com>
2015-07-04 22:05:22 +0300

    moved secrets to secrets.yml

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,7 +256,7 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :facebook, "1621467891458777", "b3813404637f26a37a574b3d195540dd"
+  config.omniauth :facebook, Rails.application.secrets.FACEBOOK_APP_ID, Rails.application.secrets.FACEBOOK_SECRET
 
-  config.omniauth :google_oauth2, "578074026093-i1ls0a4coo0gf4rr6aer7v7gp1k4uftj.apps.googleusercontent.com", "WPcLvEzxoo5xgROtWWeCZ0EE"
+  config.omniauth :google_oauth2, Rails.application.secrets.GOOGLE_CLIENT_ID, Rails.application.secrets.GOOGLE_CLIENT_SECRET
 end
