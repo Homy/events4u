2e03cd5da051ba512f41654f8987095a584cb6ca
Homy <honta.id@gmail.com>
2015-12-06 11:21:43 +0200

    added admin role to users, restricted actions on users to admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < CrudController
+  before_filter :verify_is_admin
   self.permitted_attrs = [:first_name, :last_name, :email]
+
+  private
+
+  def verify_is_admin
+    (current_user.nil?) ? redirect_to(root_path) : (redirect_to(root_path) unless current_user.admin?)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115114205) do
+ActiveRecord::Schema.define(version: 20151205154129) do
 
   create_table "event_types", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -76,12 +76,12 @@ ActiveRecord::Schema.define(version: 20151115114205) do
   add_index "reports", ["event_id"], name: "index_reports_on_event_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255,   default: "", null: false
-    t.string   "encrypted_password",     limit: 255,   default: "", null: false
+    t.string   "email",                  limit: 255,   default: "",    null: false
+    t.string   "encrypted_password",     limit: 255,   default: "",    null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,     default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,     default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 20151115114205) do
     t.string   "last_name",              limit: 255
     t.datetime "confirmed_at"
     t.text     "token",                  limit: 65535
+    t.boolean  "admin",                  limit: 1,     default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
